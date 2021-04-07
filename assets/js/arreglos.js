cc07f36245dd04abe2c9ba2c95942d1d48555299
Richard Contreras <richardcon13@gmail.com>
2021-04-07 12:42:04 +0200

    inicio con arreglos 2 curso js

---
+++ assets/js/arreglos.js
@@ -0,0 +1,23 @@
+
+
+// const arr = new Array( 10 );
+// console.log( arr );
+
+let videoJuegos = [ 'Mario 3', 'Mega', 'Crono Trigger' ];
+console.log( {videoJuegos});
+
+console.log( videoJuegos[0] );
+
+let arregloCosas = [
+   true,
+   123,
+   'Richard',
+   1 + 2, 
+   function(){},
+   ()=>{},
+   { a: 1 },
+   ['X', 'Megaman', 'Zero', 'Dr.Light', [
+       'Valeria', 'Titina']],
+];
+
+console.log( arregloCosas[7] [4] [0]);
