22510c5a1de17694b6408a653642e286a93ffe1b
Richard Contreras <richardcon13@gmail.com>
2021-04-07 11:15:08 +0200

    fin de primitivos js

---
+++ assets/js/primitivos.js
@@ -0,0 +1,30 @@
+console.log("hola");
+
+let a = 10, b = 5, c = 20, x = a + b;
+
+let nombre = 'Peter Parker';
+console.log( nombre );
+
+nombre = 'Ben Parker';
+console.log( nombre );
+
+nombre = "Tia May";
+nombre = `Tia May`;
+
+console.log( typeof nombre);
+
+let esMarvel = true;
+console.log( typeof esMarvel);
+
+let edad = 33;
+console.log( typeof edad);
+
+let superPoderDeSpiderMan;
+console.log( typeof superPoderDeSpiderMan);
+
+let soyNull = null;
+console.log( typeof soyNull);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( typeof symbol1);
